ceed51e58a13a4eaa84d03558df2b79c983c7025
Denis Tretyakov <den.tretyakoff@yandex.ru>
2024-03-26 23:51:57 +0700

    Настройки CORS.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,9 +54,11 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'payments.apps.PaymentsConfig',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'backend.urls'
 
