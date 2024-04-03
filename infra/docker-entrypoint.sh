8814844ccdbef27e951b4464e8c930e2910c2341
Denis Tretyakov <den.tretyakoff@yandex.ru>
2024-04-04 00:36:31 +0700

    Прокинул сваггер.

--- infra/docker-entrypoint.sh
+++ infra/docker-entrypoint.sh
@@ -3,4 +3,6 @@ poetry run task migrate
 poetry run task loaddata
 poetry run task createdevuser
 poetry run task loadpayments
+poetry run task collectstatic
+poetry run task copystatic
 poetry run task start
