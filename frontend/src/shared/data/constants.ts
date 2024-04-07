832313ed4faaff8a4b60b6ee153a681fe6927ce8
Kirill Kalinin <kalinin1507.2012@gmail.com>
2024-04-07 17:15:23 +0400

    env fix

--- frontend/src/shared/data/constants.ts
+++ frontend/src/shared/data/constants.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:8000/api/v1";
-// export const BASE_URL = "/api/v1";
+// export const BASE_URL = "http://localhost:8000/api/v1";
+export const BASE_URL = "/api/v1";
 
 export const months = [
   "январь",
